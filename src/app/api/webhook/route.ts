5fd8fbaccc5e0647bae4eb5b760d7b647c014733
Esmin53 <esmin.tufekcic53@gmail.com>
2024-04-10 18:32:53 +0200

    Error webhook in production

--- src/app/api/webhook/route.ts
+++ src/app/api/webhook/route.ts
@@ -6,13 +6,16 @@ import { getServerSession } from "next-auth";
 import { NextRequest, NextResponse } from "next/server";
 import Stripe from "stripe";
 
+export const config = {  api: {    bodyParser: false,  },};
 
-export const POST = async (req: NextRequest, res: NextResponse) => {
+export const POST = async (req: NextRequest) => {
 
     const payload = await req.text()
     const response = await JSON.parse(payload)
 
-    const sig = req.headers.get('stripe-signature') as string;
+    const sig = req.headers.get('stripe-signature')
+
+    console.log("PAYLOAD: ", payload)
 
     const dateTime = new Date(response?.created * 1000).toLocaleDateString()
     const timeString = new Date(response?.created * 1000).toLocaleDateString()
@@ -29,23 +32,26 @@ export const POST = async (req: NextRequest, res: NextResponse) => {
         if (!session?.metadata?.userId || !session?.metadata?.orderId) {
             return new Response(JSON.stringify("No metadata"), { status: 400 })
             }
+        
+        if (event.type === 'checkout.session.completed') {
+            const [user] = await db.select().from(users).where(eq(users.id, session.metadata.userId))
 
-        const [user] = await db.select().from(users).where(eq(users.id, session.metadata.userId))
-
-        if(!user) {
-            return new Response(JSON.stringify("No such user"), { status: 404 } );
+            if(!user) {
+                return new Response(JSON.stringify("No such user"), { status: 404 } );
+            }
+    
+            const [order] = await db.select().from(orders).where(eq(orders.id, session.metadata.orderId))
+    
+            if(!order) {
+                return new Response(JSON.stringify("No such order"), { status: 404 })
+            }
+    
+                await db.update(orders).set({
+                    isPaid: true,
+                    orderStatus: "payment_successful"
+                }).where(eq(orders.id, session.metadata.orderId))
         }
 
-        const [order] = await db.select().from(orders).where(eq(orders.id, session.metadata.orderId))
-
-        if(!order) {
-            return new Response(JSON.stringify("No such order"), { status: 404 })
-        }
-
-            await db.update(orders).set({
-                isPaid: true,
-                orderStatus: "payment_successful"
-            }).where(eq(orders.id, session.metadata.orderId))
 
             return NextResponse.json({ status: 200, event: event.type})
     } catch (error) {
